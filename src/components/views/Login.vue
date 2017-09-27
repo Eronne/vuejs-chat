2d6d4a91161cce4e17002811dbea2afb9fc1c06d
Eronne <erwann.letue@gmail.com>
2017-09-28 00:23:48 +0200

    Login view appearance

--- src/components/views/Login.vue
+++ src/components/views/Login.vue
@@ -1,11 +1,15 @@
 <template>
   <main>
-    <h1 v-if="store.user">{{ store.user.name }}</h1>
-    <form @submit.prevent="onSubmit">
+    <div class="container">
+      <img src="../../assets/img/login/logo.png" alt="Logo" class="logo">
+      <h1 v-if="store.user">{{ store.user.name }}</h1>
       <p @class="error" v-if="error">Username should be between 1 and 15 characters</p>
-      <input type="text" placeholder="Username" v-model="username">
-      <button>Se connecter</button>
-    </form>
+
+      <form @submit.prevent="onSubmit">
+        <input type="text" placeholder="Username" v-model="username">
+        <button>Connexion</button>
+      </form>
+    </div>
   </main>
 </template>
 
@@ -31,3 +35,63 @@
   }
 </script>
 
+<style lang="scss" scoped="">
+  main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+
+    .container {
+      position: relative;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 30%;
+      height: 55%;
+      border: 1px solid white;
+      border-radius: 3px;
+
+      @media (max-width: 980px) {
+        width: 60%;
+      }
+
+      .logo {
+        position: absolute;
+        top: -56px;
+      }
+
+      form{
+        display: block !important;
+
+        ::placeholder{
+          color: #a5a5a5;
+          text-transform: uppercase;
+        }
+
+        input, button {
+          display: block;
+        }
+
+        input {
+          text-align: center;
+          padding: 8px;
+          margin-bottom: 30px;
+          color: #ffffff;
+          border: none;
+          border-bottom: 1px solid white;
+          background-color: transparent;
+        }
+
+        button {
+          margin: 0 auto;
+          padding: 10px;
+          color: #ffffff;
+          background-color: transparent;
+          border: 1px solid white;
+          border-radius: 3px;
+        }
+      }
+    }
+  }
+</style>
