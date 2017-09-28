6a8320b3e6f0ea792b6c6b9def90fedbba4e8773
Eronne <erwann.letue@gmail.com>
2017-09-28 20:07:26 +0200

    Differencing messages

--- src/components/messages/Message.vue
+++ src/components/messages/Message.vue
@@ -1,5 +1,5 @@
 <template>
-  <article>
+  <article v-bind:class="[classUser ? 'current-user': 'other-user']">
     <p>User : {{data.author.username}}</p>
     <p>Time : {{data.createdAt}}</p>
     <p>Content : {{ data.body }}</p>
@@ -10,6 +10,23 @@
 <script>
   export default {
     name: 'message',
-    props: ['data']
+    props: ['data'],
+    data () {
+      return {
+        classUser: false
+      }
+    },
+    methods: {
+      convertData () {
+        let userId = this.data.author.id
+        let storeId = this.$store.user.id
+        if (userId === storeId) {
+          this.classUser = true
+        }
+      }
+    },
+    created () {
+      this.convertData()
+    }
   }
 </script>
